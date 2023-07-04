12bdcc150c89bbbf82a297cfd34224c48bb3c01a
Guilherme Silva <guilherme92325@gmail.com>
2023-07-04 16:36:23 -0300

    feat: add auth microservice

---
+++ ms-auth/src/test/java/br/com/compassuol/pb/challenge/msauth/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compassuol.pb.challenge.msauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
