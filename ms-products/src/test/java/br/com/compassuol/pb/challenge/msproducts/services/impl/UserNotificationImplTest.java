b8af56d7f634fb66dfccc1a1b11eee3ec7a755e4
Guilherme Silva <guilherme92325@gmail.com>
2023-07-08 13:16:33 -0300

    tests(products): refactor test to notification service

--- ms-products/src/test/java/br/com/compassuol/pb/challenge/msproducts/services/impl/UserNotificationImplTest.java
+++ ms-products/src/test/java/br/com/compassuol/pb/challenge/msproducts/services/impl/UserNotificationImplTest.java
@@ -1,6 +1,6 @@
 package br.com.compassuol.pb.challenge.msproducts.services.impl;
 
-import br.com.compassuol.pb.challenge.msproducts.entities.dto.NotificationDto;
+import br.com.compassuol.pb.challenge.msproducts.entities.dto.EmailDto;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,7 +10,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 
+import static br.com.compassuol.pb.challenge.msproducts.constants.EmailConstants.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.verify;
 
@@ -23,6 +26,14 @@ class UserNotificationImplTest {
     @InjectMocks
     UserNotificationImpl userNotification;
 
+    String EMAIL = "email@email.com";
+    String NAME = "name";
+
+
+    @BeforeEach
+    void setUp() {
+    }
+
     @AfterEach
     void tearDown() {
         reset(rabbitTemplate);
@@ -30,15 +41,29 @@ class UserNotificationImplTest {
 
     @Test
     void sendNewUserNotification() {
-        userNotification.sendNewUserNotification("email", "name");
+        userNotification.sendNewUserNotification(EMAIL, NAME);
 
-        verify(rabbitTemplate).convertAndSend("exchange", "user_notif", new NotificationDto("name", "email", "NEW_USER"));
+        verify(rabbitTemplate).convertAndSend(eq("exchange"), eq("user_notif"), any(EmailDto.class));
     }
 
     @Test
     void sendUserModifiedNotification() {
-        userNotification.sendUserModifiedNotification("email", "name");
+        userNotification.sendUserModifiedNotification(EMAIL, NAME);
 
-        verify(rabbitTemplate).convertAndSend("exchange", "user_notif", new NotificationDto("name", "email", "USER_MODIFIED"));
+        verify(rabbitTemplate).convertAndSend(eq("exchange"), eq("user_notif"), any(EmailDto.class));
+    }
+
+    @Test
+    void generateTemplate() {
+        EmailDto emailDto = userNotification.generateTemplate(EMAIL, NAME, "NEW_USER");
+
+        assertEquals(EMAIL_SENDER, emailDto.fromEmail());
+        assertEquals(EMAIL_NAME, emailDto.fromName());
+        assertEquals(NAME, emailDto.replyTo());
+        assertEquals(EMAIL, emailDto.to());
+        assertEquals(EMAIL_SUBJECT_NEW_USER, emailDto.subject());
+        assertEquals(String.format(EMAIL_BODY_NEW_USER, NAME), emailDto.body());
+        assertEquals(EMAIL_CONTENT_TYPE_HTML, emailDto.contentType());
+
     }
 }
